51793d65c914c8d134873faf7254939a66bb72a7
Dragan Ćulibrk <33953768+draganculibrk9@users.noreply.github.com>
2019-05-21 22:53:00 +0200

    Created project and basic classes

---
+++ Projekat/model/Etiketa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekat.model
+{
+    public class Etiketa
+    {
+        public string Oznaka { get; set; }
+        public string Boja { get; set; }
+        public string Opis { get; set; }
+    }
+}
---
+++ Projekat/model/Vrsta.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekat.model
+{
+    public class Vrsta
+    {
+        public string Oznaka { get; set; }
+        public string Ime { get; set; }
+        public string Opis { get; set; }
+        public string Tip { get; set; }
+        public StatusUgrozenosti StatusUgrozenosti { get; set; }
+        public string Ikonica { get; set; }
+        public bool Opasna { get; set; }
+        public bool IUCN { get; set; }
+        public bool ZiviUNaseljenomRegionu { get; set; }
+        public TuristickiStatus TuristickiStatus { get; set; }
+        public double GodisnjiPrihod { get; set; }
+        public DateTime DatumOtkrivanja;
+        List<Etiketa> Etikete { get; set; }
+    }
+}
